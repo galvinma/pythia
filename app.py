4181972055cb8cf3c8831631d8d6efaee109bf16
galvinma <mattgalvin47@gmail.com>
2015-09-25 20:26:21 -0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def text():
 		return "Welcome to the Pythia Project!"
 
+print(os.environ['APP_SETTINGS'])
+
 if __name__ == '__main__':
 	app.run()
+
+
