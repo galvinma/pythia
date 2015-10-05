f1b7e8c71142fe21fee40ffad19bbcd86085bb7a
galvinma <mattgalvin47@gmail.com>
2015-10-04 21:15:01 -0700

    Cleaning up file structure. Working fresh on flask integration. Once I complete signup page, I will integrate with postgres. Doing both at the same time is confusing.

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-import flask_login 
 import os
-import settings
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
 from sqlalchemy.engine.url import URL
-from flask_wtf import Form
-from wtforms import validators, StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Email, Length, Required
-from flask_login import LoginManager
-from config import Config
-from model import User
-from signup import SignupForm
+
+
 
 
 app = Flask(__name__)
@@ -19,37 +12,16 @@ db = SQLAlchemy()
 app.config.from_object(os.environ['APP_SETTINGS'])
 db.init_app(app)
 
-def db_connect():
-    return create_engine(URL(settings.DATABASE))
-
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-
-
 
 def create_app(app):
-
-	@login_manager.user_loader
-	def load_user(id):
-		return User.get(id)
-
-	login_manager.setup_app(app)
 
 	@app.route('/')
 	def index():
 		return render_template('index.html')
 
-	@app.route('/signup', methods=['GET', 'POST'])
+	@app.route('/signup')
 	def sign_up():
-		form = SignupForm()
-		return render_template('signup.html', email='email')
-
-	@app.route('/about')
-	def about():
-		return render_template('about.html')
+		return render_template('signup.html')
 
 	@app.route('/search')
 	def search():
