9a8c28beeda04fda794f70d7c0c4b916e05a1574
Matthew Galvin <mattgalvin47@gmail.com>
2017-05-19 07:39:57 -0700

    working on cluster

--- cluster.py
+++ cluster.py
@@ -2,31 +2,46 @@ import numpy as np
 import pandas as pd
 import math
 from kmodes import kmodes
+	
+# Similarity function compares two DataFrame objects and returns a similarity score
+	# ex. 
+	# a = [1,0,0,0], b = [1,1,0,0]
+	# delta = abs(a - b) = [0,1,0,0]
+	# similarity = sum(delta) = 1
+# Lower similarity scores imply a object is more similar
 
-def similarity(user, cluster):
-	similarity = pd.DataFrame(user-cluster).abs()
+def similarity(object_one, object_two):
+	delta = pd.DataFrame(onject_one-object_two).abs()
+	similarity = 0
+	for i in delta:
+		similarity += i
 	return similarity
 
 def cluster():
 	# Find population size
 	population = session.query(func.count(User.id))
+
 	# Get a list of all interests (ids)
 	interests = []
 	get_interest_ids = session.query(Interests).filter(Interests.id != None)
 	for interest in get_interest_ids.all():
 		interests.append(interest.id)
+
 	# Get list of all users (ids)
 	users = []
 	get_user_ids = session.query(User).filter(User.id != None)
 	for user in get_user_ids.all():
 		users.append(user.id)
+
 	# Get a list of all user-interest combinations
 	user_interests = []
 	get_user_interests = session.query(UserInterests).filter(UserInterests.user_id != None)
 	for pair in get_user_interests.all():
 		user_interests.append([UserInterests.user_id, UserInterests.interest_id)
+
 	# Generate np zeros matrix of interests x users
 	data = pd.DataFrame(np.zeros((len(users)),len(interests)), index=users, columns=interests)
+
 	# Fill in user interests
 	for pair in user_interests:
 		row = pair[0]
@@ -39,8 +54,10 @@ def cluster():
 	# Total Population (tp) = K (# of clusters) * cs (average cluster size)
 	k_min = math.floor(population/500)
 	k_max = math.floor(population/25)
+
 	# Item in list k represents an attempt to find the best fit number of clusters  
 	k = []
+
 	# Create optimal spacing based on estimate of k_min
 	i = k_min
 	while i < 50 and i < k_max:
@@ -72,12 +89,42 @@ def cluster():
 	ks = []
 
 	for num in k:
+		# init kmodes algo with num clusters
 		kmodes_cao = kmodes.KModes(n_clusters=num, init='Cao')
 		kmodes_cao.fit_predict(data)
-		# Create list of UserIntersts--Centroid
+
+		# Create a DataFrame linking cluster id to cluster centroid
+		# NOTE: Default DataFrame index will match Kmodes cluster label
 		clusters = pd.DataFrame(kmodes_cao.cluster_centroids_)
+
+		# Create a DataFrame linking user id to cluster label
 		user_cluster = pd.DataFrame(index=data.index)
 		user_cluster['clusters'] = kmodes_cao.labels_
+
+		# Create a DataFrame for each cluster and add it to dictionary
+		users_sorted = {}
+		for index, row in user_cluster.iterrows():
+			if row not in users_sorted:
+				users_sorted['index']
+
+
+		# Calculate the silhouette coefficient for each point
+		#
+		# For the ith obj, calculate its average distance to all other objects in the cluster \\
+		# Call this value, "ai".
+		#
+		# For the ith obj, and any cluster not containing the obj, calculate the object's average \\
+		# distance to all the objects in the given cluster. Find the minimum such value with \\
+		# respect to all clusters; call this value, "bi"
+		#
+		# For the ith obj, the silhouette coefficient is defined as:
+		# si = (bi-ai)/max(ai,bi)
+
+
+
+
+
+
 		# Pass user and associated cluster to similarity function
 		for index, row in user_cluster.iterrows():
 			cluster = clusters.iloc[row['clusters']]
@@ -85,8 +132,3 @@ def cluster():
 			# Find similarity between user and cluster
 			sim = similarity(user, cluster)
 			 
-
-
-
-
-		# Need User-Interests and Associated Cluster
