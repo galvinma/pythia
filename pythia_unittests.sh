ca2a632fbb867566cc30a5b28bd884bc02bfc2bb
Matthew Galvin <mattgalvin47@gmail.com>
2017-10-16 10:42:48 -0700

    final jenkins tweaks

--- pythia_unittests.sh
+++ pythia_unittests.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo "Changing directory and pulling from source.."
+echo "Changing directory and pulling from source..."
 repo="git@github.com:galvinma/pythia.git"
-git init .
 if [ -d ".git" ]; then
-  echo "This directory has already been initialized with git."
+  echo "This directory has already been initialized with git. Pulling latest master..."
   git pull origin master
 else
+  git init .
   git remote add origin "https://github.com/galvinma/pythia.git"
   git pull origin master
 fi
 
+echo "Removing old virtualenv..."
+rm -rf "/home/galvinma/.virtualenvs/jenkinspythia/"
+
+echo "Creating directory if DNE..."
+if [ ! -d "/home/galvinma/.jenkins/workspace/pythia/" ]; then
+  mkdir -p "/home/galvinma/.jenkins/workspace/pythia/"
+fi
+
 echo "Creating virtual env and installing requirements..."
-mkvirtualenv jenkinspythia -a "/home/galvinma/.jenkins/workspace/pythia/"
+cd "/home/galvinma/.jenkins/workspace/pythia/"
+pip install virtualenv
+virtualenv jenkinspythia
+cd "/home/galvinma/.jenkins/workspace/pythia/jenkinspythia/bin/"
+source activate
+
+cd "/home/galvinma/.jenkins/workspace/pythia/"
 pip install -r requirements.txt
 
 echo "Executing Pythia and associated tests..."
-ls
+python view.py &
 python sel.py
 exit 0
